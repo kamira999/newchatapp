2c545b15618a95569c6382c2065b6ce2725c64db
PeytonJohnson999 <Johnso630@stcharles.k12.la.us>
2026-09-02 10:03:39 -0500

    0.0.0c

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rustls::{ServerConfig, pki_types::{CertificateDer, PrivateKeyDer, pem::PemObject}};
+
+fn main() -> anyhow::Result<()> {
+
+    let cert_file = "./.certs/cert.pem";
+    let private_key_file = "./.certs/private.pem";
+
+    //TODO: FIX "Error: no items found" ON CERT AND KEY
+    // MAYBE GEN NEW KEYS?? DIFF ENCODING? DER? SOMETHING SOMETHING IDK
+    let certs: Vec<_> = CertificateDer::pem_file_iter(".certs/certificate.pem")?
+        .map(|cert| cert.unwrap())
+        .collect();
+
+
+    let key = PrivateKeyDer::from_pem_file(".certs/myPrivateKey.key")?;
+
+
+    Ok(())
 }
